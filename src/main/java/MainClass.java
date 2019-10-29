a629302b803e6425e8f77ca3c512736b5153a9aa
fede4ka <fed0t@mail.ru>
2019-10-29 16:22:38 +0300

    filter added

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -4,30 +4,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class MainClass {
-
-    public static void main(String[] args) throws MalformedURLException {
-        URL url = new URL( "https://yande.re/post.json?tags=azur_lane;limit=50");
+    public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
         try {
-           Picture[] pics = mapper.readValue(url, Picture[].class);
+            URL url = new URL("https://yande.re/post.json?tags=azur_lane;limit=100");
+            Picture[] pics = mapper.readValue(url, Picture[].class);
+            Predicate<Picture> byRating = picture -> picture.getRating() != 'e';
+            pics = Arrays.stream(pics).filter(byRating).limit(1).toArray(Picture[]::new);
             new File(Paths.get(".").toAbsolutePath().normalize().toString() + "/pics").mkdirs();
             for (Picture p : pics) {
                 System.out.println(Picture.namebuilder(p));
-
+                System.out.println(p.getRating());
                 URL picurl = new URL(p.getJpeg_url());
                 try (
                         ReadableByteChannel rbc = Channels.newChannel(picurl.openStream());
-                        FileOutputStream fos = new FileOutputStream("pics/"+ Picture.namebuilder(p)+".jpeg");
-                     )
-                {
+                        FileOutputStream fos = new FileOutputStream("pics/" + Picture.namebuilder(p) + ".jpeg")
+                ) {
                     fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
                 }
 
--- src/main/java/Picture.java
+++ src/main/java/Picture.java
@@ -1,46 +1,64 @@
-
 public class Picture {
     public int id;
     public String tags;
     public String jpeg_url;
+    public char rating;
+
+    public Picture(int id, String tags, String jpeg_url, char rating) {
+        this.id = id;
+        this.tags = tags;
+        this.jpeg_url = jpeg_url;
+        this.rating = rating;
+    }
 
     public Picture() {
         super();
     }
-    public Picture(int id, String tags, String jpeg_url) {
-        this.id = id;
-        this.tags = tags;
-        this.jpeg_url = jpeg_url;
+
+    public static String namebuilder(Picture pic) {
+
+        String tags = pic.getTags();
+        String[] arr;
+        if (tags.length() > 150) {
+            arr = tags.split("\\s+");
+            StringBuilder tagsshort = new StringBuilder();
+            for (int i = 0; i < 5; i++) {
+                tagsshort.append(" ").append(arr[i]);
+            }
+            tags = tagsshort.toString();
+        }
+        return "yande.re" + " " + pic.getId() + " " + tags.replaceAll("[/|.|\\|]+", "_");
     }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public void setTags(String tags) {
-        this.tags = tags;
-    }
-    public void setJpeg_url(String jpeg_url) {
-        this.jpeg_url = jpeg_url;
-    }
+
     public int getId() {
         return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTags() {
         return tags;
     }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
     public String getJpeg_url() {
         return jpeg_url;
     }
-    public static String namebuilder (Picture pic) {
 
-        String tags = pic.getTags();
-        String [] arr;
-        if (tags.length()>150)  {
-            arr = tags.split("\\s+");
-            String tagsshort="";
-            for(int i=0; i<5 ; i++){
-                tagsshort = tagsshort + " " + arr[i] ;
-            }
-            tags = tagsshort;}
-        return "yande.re" +" " +pic.getId() +" " + tags.replaceAll("[/|.|\\|]+", "_") ;
+    public void setJpeg_url(String jpeg_url) {
+        this.jpeg_url = jpeg_url;
+    }
+
+    public char getRating() {
+        return rating;
+    }
+
+    public void setRating(char rating) {
+        this.rating = rating;
     }
 }
