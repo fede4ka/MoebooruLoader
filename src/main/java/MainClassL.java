11d491c8a121c136317a97ac5a3132f15263c270
fede4ka <fed0t@mail.ru>
2019-11-20 23:24:27 +0300

    Loan pattern

--- src/main/java/MainClassL.java
+++ src/main/java/MainClassL.java
@@ -2,7 +2,7 @@
 public class MainClassL {
     public static void main(String[] args) {
         LoanLoader.StartLoading(Loader ->
-                Loader.tags("azur_lane")
+                  Loader.tags("azur_lane")
                         .number(50));
     }
 }
