eac0e9bc210bead34f5568495badf9b625490dd5
fede4ka <40442383+fede4ka@users.noreply.github.com>
2020-01-05 18:35:32 +0300

    Delete MainClassL.java

--- src/main/java/MainClassL.java
+++
@@ -1,8 +0,0 @@
-//using loan pattern
-public class MainClassL {
-    public static void main(String[] args) {
-        LoanLoader.StartLoading(Loader ->
-                Loader.tags("azur_lane")
-                        .number(50));
-    }
-}
